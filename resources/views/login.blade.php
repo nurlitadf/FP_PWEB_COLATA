c128d463962d1b03ac9c7a282730f20973f1d46f
Nurlita <nurlitadf17@gmail.com>
2018-05-19 23:12:33 +0700

    add existing file

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	
+</head>
+<body>
+	<form action="/login_process.php">
+	  	Username:<br>
+	  	<input type="text" name="username"><br>
+	  	Password:<br>
+	  	<input type="password" name="password">
+	  	<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
